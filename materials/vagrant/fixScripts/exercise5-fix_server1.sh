e2eaa23a0d3973bacdf571e5aeeaba3ed58b12fb
Vikaben <61282639+Vikaben@users.noreply.github.com>
2022-01-07 17:46:37 +0200

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
 
-#------server1------
 #generate ssh key to server1
-ssh-keygen -t rsa -b 4096 -C root@server1
 
-#go to /.ssh/id_rsa.pub and copy the ssh key
+sh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1s
 
-#------server2------
-#Copy from server1 id_rsa.pub to /.ssh/authorized_keys 
+
 
 
