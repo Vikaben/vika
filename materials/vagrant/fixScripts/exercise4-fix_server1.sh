0878278965ff9a585782b91701fff28d7e9ce061
Vikaben <61282639+Vikaben@users.noreply.github.com>
2022-01-07 17:45:09 +0200

    Update exercise4-fix_server1.sh

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -2,7 +2,7 @@
 #add fix to exercise4-server1 here
 # Need to add to server2 to /etc/host 192.168.100.11 server2 server2
 
-#and remove PasswordAuthentication 
-vi /etc/ssh/sshd_config 
-#PasswordAuthentication no
+sed -i "192.168.100.11 server2 server2" /etc/hosts
 
+#and remove PasswordAuthentication 
+cat /etc/ssh/sshd_config  | grep -v  "PasswordAuthentication no"  >> /etc/ssh/sshd_config
