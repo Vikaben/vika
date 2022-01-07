6808a0763af690db114bda722fe0c65d02c480df
Vikaben <61282639+Vikaben@users.noreply.github.com>
2022-01-07 17:46:47 +0200

    Update exercise3-fix.sh

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -2,10 +2,7 @@
 #add fix to exercise3 here
 #Delete from 000-default.conf "require all denied"
 
-vi /etc/apache2/sites-enabled/000-default.conf
-#        <Location "/">
-#          Require all denied
-#                 </Location>
 
+cat /etc/apache2/sites-enabled/000-default.conf | grep -v "Require all denied" >> /etc/apache2/sites-enabled/000-default.conf 
 
 
